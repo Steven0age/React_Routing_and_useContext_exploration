b423c26399cea9c26115a4de3c98431677bea258
Stephan Haak <info@stephan-haak.com>
2025-06-15 11:33:19 +0200

    set deploy.yaml to node-version 20 and dependency to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/React_Routing_and_useContext_exploration/",
+export default defineConfig(({ mode }) => ({
+  base:
+    mode === "production" ? "/React_Routing_and_useContext_exploration/" : "/",
   plugins: [react()],
-});
+}));
